6dbcd187bb4209d6a10136749306d176c8b65b51
xpomin <xpomin@pop-os.localdomain>
2023-02-19 00:02:30 +0700

    fix

--- src/SecantMethod.hpp
+++ src/SecantMethod.hpp
@@ -6,13 +6,13 @@ template <float_like T = float>
 class SecantMethod : public BaseMethod<T> {
  public:
   SecantMethod(std::function<T(T)> function, std::function<T(T)> derivative,
-               T epsilon = 0.001)
+               T epsilon = 0.0000001)
       : BaseMethod<T>(function, derivative, epsilon) {}
 
   T Solution(T l_bound, T r_bound, bool print_intermediate_results = false,
              std::ostream& os = std::cout) {
-    T xn_minus_2 = SecantFormula(l_bound, r_bound);
-    T xn_minus_1 = SecantFormula(xn_minus_1, r_bound);
+    T xn_minus_2 = SecantFormula(r_bound, l_bound);
+    T xn_minus_1 = SecantFormula(xn_minus_2, r_bound);
     T xn = SecantFormula(xn_minus_1, xn_minus_2);
 
     if (print_intermediate_results) {
@@ -20,9 +20,6 @@ class SecantMethod : public BaseMethod<T> {
     }
 
     while (!this->isEqual(xn, xn_minus_1)) {
-      if (print_intermediate_results) {
-        os << "xn-1 = " << xn_minus_1 << " xn = " << xn << '\n';
-      }
       if (xn != xn) break;
 
       auto buf1 = xn_minus_1;
@@ -31,11 +28,15 @@ class SecantMethod : public BaseMethod<T> {
       xn_minus_2 = xn_minus_1;
       xn_minus_1 = xn;
       xn = SecantFormula(buf1, buf2);
+
+      if (print_intermediate_results) {
+        os << "xn-1 = " << xn_minus_1 << " xn = " << xn << '\n';
+      }
     }
     return xn;
   }
 
-  T* Solution(T* bounds, int bounds_count,
+  T* Solution(T* bounds, int bounds_count, T step = 0.1,
               bool print_intermediate_results = false,
               std::ostream& os = std::cout) {
     T* result = new T[bounds_count / 2];
@@ -45,7 +46,7 @@ class SecantMethod : public BaseMethod<T> {
       if (print_intermediate_results) os << "#" << i + 1 << '\n';
 
       result[i] =
-          Solution(bounds[j], bounds[j] + 0.01, print_intermediate_results, os);
+          Solution(bounds[j], bounds[j] + step, print_intermediate_results, os);
     }
     return result;
   }
@@ -55,7 +56,7 @@ class SecantMethod : public BaseMethod<T> {
     auto diff1 = xn_minus_1 - xn_minus_2;
     auto diff2 = this->function(xn_minus_1) - this->function(xn_minus_2);
     if (diff1 < this->epsilon) return xn_minus_1;
-    if (fabsf(diff2) < this->epsilon) throw std::runtime_error("asd");
+    // if (fabsf(diff2) < this->epsilon) throw std::runtime_error("nan");
     return xn_minus_1 - (this->function(xn_minus_1) * diff1 / diff2);
   }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "NewtonMethod.hpp"
 #include "SecantMethod.hpp"
 
-#define EPSILON 1e-4
+#define EPSILON 1e-3
 
 template <float_like T = float>
 T function(T x) {
@@ -18,6 +18,16 @@ template <float_like T = float>
 T derivative(T x) {
   return -2 * x + 7 - 4 / x;
 }
+
+template <float_like T = float>
+T function2(T x) {
+  return 100 * x * x - 1000 * x - 6;
+}
+template <float_like T = float>
+T derivative2(T x) {
+  return 200 * x - 1000;
+}
+
 void PrintTitle(std::string title, std::ostream& os = std::cout) {
   os << "\n****" << title << "****" << '\n';
 }
@@ -70,10 +80,11 @@ void IterationTest(T* bounds, int bounds_count, std::function<T(T)> function,
 template <float_like T = float>
 void SecantTest(T* bounds, int bounds_count, std::function<T(T)> function,
                 std::function<T(T)> derivative, T* answers = NULL,
-                bool printAll = false, std::ostream& os = std::cout) {
+                bool printAll = false, std::ostream& os = std::cout,
+                T step = 0.001) {
   PrintTitle("SECANT METHOD TEST", os);
   auto method = SecantMethod<T>(function, derivative);
-  auto result = method.Solution(bounds, bounds_count, printAll, os);
+  auto result = method.Solution(bounds, bounds_count, step, printAll, os);
   if (result == NULL) throw std::runtime_error("Null back");
 
   PrintAnswers(answers, result, bounds_count / 2, os);
@@ -81,34 +92,51 @@ void SecantTest(T* bounds, int bounds_count, std::function<T(T)> function,
 }
 
 void Task1(bool printAll = false, std::ostream& os = std::cout) {
-  os << "TASK 1 fun: -x^2 + 7x - 4ln(x) - 7\n";
+  os << "\nTASK 1 fun: -x^2 + 7x - 4ln(x) - 7\n";
   std::function<float(float)> fun = function<float>;
   std::function<float(float)> der = derivative<float>;
 
   float answers[] = {0.2766, 1.7925, 3.6841};
-  float bounds[] = {0.2, 1.0, 1.0, 2.0, 3.0, 4.0};
+  float bounds[] = {0.000001, 1.0, 1.0, 2.0, 3.0, 4.0};
 
   NewtonTest(bounds, 6, fun, der, answers, printAll, os);
   IterationTest(bounds, 6, fun, der, answers, printAll, os);
-  SecantTest(bounds, 6, fun, der, answers, printAll, os);
+  SecantTest(bounds, 6, fun, der, answers, printAll, os, 0.1f);
+}
+
+void Task2(bool printAll = false, std::ostream& os = std::cout) {
+  os << "\nTASK 2 fun: 100x^2 - 1000x -6\n";
+  std::function<float(float)> fun = function2<float>;
+  std::function<float(float)> der = derivative2<float>;
+
+  float answers[] = {-0.006, 10.006};
+  float bounds[] = {-1, 0, 10, 11};
+  NewtonTest(bounds, 4, fun, der, answers, printAll, os);
+  IterationTest(bounds, 4, fun, der, answers, printAll, os);
+  SecantTest(bounds, 4, fun, der, answers, printAll, os);
 }
 
 int main() {
-  // std::ofstream os;
-  // os.open("Results.txt", std::ofstream::out | std::ofstream::trunc);
-  // Task1(true, os);
-  // os.close();
+  std::ofstream os;
+
+  os.open("Task1.txt", std::ofstream::out | std::ofstream::trunc);
+  Task1(false, os);
+  os.close();
+
+  os.open("Task1+.txt", std::ofstream::out | std::ofstream::trunc);
+  Task1(true, os);
+  os.close();
+
+  os.open("Task2.txt", std::ofstream::out | std::ofstream::trunc);
+  Task2(false, os);
+  os.close();
+
+  os.open("Task2+.txt", std::ofstream::out | std::ofstream::trunc);
+  Task2(true, os);
+  os.close();
+
   Task1();
-  // float answers[] = {-1.4404, 1.61657, 8.43279};
-  // float bounds[] = {-2, -1, 1, 2, 8, 9};
-
-  // float bounds[] = {-4, -3, -2, -1, 0, 1, 4, 5};
-
-  // NewtonTest(bounds, 8, fun, der);
-  // IterationTest(bounds, 8, fun, der);
-  // SecantTest(bounds, 8, fun, der);
-
-  //*****************
+  Task2();
 
   return 0;
 }
