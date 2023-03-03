e163d332f40b601ac6782b31bea20e83dc1fb1b5
danilka108 <danilka108@yandex.ru>
2023-03-03 23:13:53 +0700

    fix secant method

--- src/BaseMethod.hpp
+++ src/BaseMethod.hpp
@@ -6,9 +6,8 @@ template <typename T = float>
 concept float_like = std::is_same_v<T, float> || std::is_same_v<T, double> ||
                      std::is_same_v<T, long double>;
 
-template <float_like T = float>
-class BaseMethod {
- public:
+template <float_like T = float> class BaseMethod {
+public:
   BaseMethod(std::function<T(T)> function, std::function<T(T)> derivative,
              T epsilon) {
     this->function = function;
@@ -20,24 +19,30 @@ class BaseMethod {
 
   T getEpselon() { return this->epsilon; }
   void setEpselon(T newEpsilon) {
-    if (newEpsilon < 1e-30) throw std::runtime_error("too small epsilon");
-    if (newEpsilon < 0) throw std::runtime_error("negative epsilon");
+    if (newEpsilon < 1e-30)
+      throw std::runtime_error("too small epsilon");
+    if (newEpsilon < 0)
+      throw std::runtime_error("negative epsilon");
     this->epsilon = newEpsilon;
   }
 
- protected:
+protected:
   T epsilon;
   std::function<T(T)> function;
   std::function<T(T)> derivative;
+
   bool isEqual(T val1, T val2) {
     if (t_abs(val1 - val2) < this->epsilon)
       return true;
     else
       return false;
   }
+
   T t_abs(T x) {
-    if (x < 0) x *= -1;
+    if (x < 0)
+      x *= -1;
     return x;
   }
-  void printIntermTitle(std::ostream& os) { os << "\nIntermediate Results\n"; }
+
+  void printIntermTitle(std::ostream &os) { os << "\nIntermediate Results\n"; }
 };
--- src/SecantMethod.hpp
+++ src/SecantMethod.hpp
@@ -2,48 +2,51 @@
 
 #include "BaseMethod.hpp"
 
-template <float_like T = float>
-class SecantMethod : public BaseMethod<T> {
- public:
+template <float_like T = float> class SecantMethod : public BaseMethod<T> {
+public:
   SecantMethod(std::function<T(T)> function, std::function<T(T)> derivative,
-               T epsilon = 0.0000001)
+               T epsilon = 0.001)
       : BaseMethod<T>(function, derivative, epsilon) {}
 
   T Solution(T l_bound, T r_bound, bool print_intermediate_results = false,
-             std::ostream& os = std::cout) {
-    T xn_minus_2 = SecantFormula(r_bound, l_bound);
-    T xn_minus_1 = SecantFormula(xn_minus_2, r_bound);
-    T xn = SecantFormula(xn_minus_1, xn_minus_2);
+             std::ostream &os = std::cout) {
+    T xn = l_bound;
+    T xn_1 = r_bound;
+    // T xn = SecantFormula(xn_minus_1, xn_minus_2);
 
     if (print_intermediate_results) {
-      os << "xn-1 = " << xn_minus_1 << " xn = " << xn << '\n';
+      os << "xn = " << xn << " xn + 1 = " << xn_1 << '\n';
     }
 
-    while (!this->isEqual(xn, xn_minus_1)) {
-      if (xn != xn) break;
+    while (!this->isEqual(xn, xn_1)) {
+      // if (xn != xn) break;
 
-      auto buf1 = xn_minus_1;
-      auto buf2 = xn_minus_2;
+      auto buf = xn_1;
+      // auto buf2 = xn_minus_2;
 
-      xn_minus_2 = xn_minus_1;
-      xn_minus_1 = xn;
-      xn = SecantFormula(buf1, buf2);
+      // xn_minus_2 = xn_minus_1;
+      // xn_minus_1 = xn;
+      xn_1 = SecantFormula(xn, xn_1);
+      xn = buf;
 
       if (print_intermediate_results) {
-        os << "xn-1 = " << xn_minus_1 << " xn = " << xn << '\n';
+        os << "xn = " << xn << " xn + 1 = " << xn_1 << '\n';
       }
     }
+
     return xn;
   }
 
-  T* Solution(T* bounds, int bounds_count, T step = 0.1,
+  T *Solution(T *bounds, int bounds_count, T step = 0.1,
               bool print_intermediate_results = false,
-              std::ostream& os = std::cout) {
-    T* result = new T[bounds_count / 2];
-    if (print_intermediate_results) this->printIntermTitle(os);
+              std::ostream &os = std::cout) {
+    T *result = new T[bounds_count / 2];
+    if (print_intermediate_results)
+      this->printIntermTitle(os);
 
     for (int i = 0, j = 0; i < bounds_count / 2; i++, j += 2) {
-      if (print_intermediate_results) os << "#" << i + 1 << '\n';
+      if (print_intermediate_results)
+        os << "#" << i + 1 << '\n';
 
       result[i] =
           Solution(bounds[j], bounds[j] + step, print_intermediate_results, os);
@@ -51,12 +54,15 @@ class SecantMethod : public BaseMethod<T> {
     return result;
   }
 
- private:
-  T SecantFormula(T xn_minus_1, T xn_minus_2) {
-    auto diff1 = xn_minus_1 - xn_minus_2;
-    auto diff2 = this->function(xn_minus_1) - this->function(xn_minus_2);
-    if (diff1 < this->epsilon) return xn_minus_1;
-    // if (fabsf(diff2) < this->epsilon) throw std::runtime_error("nan");
-    return xn_minus_1 - (this->function(xn_minus_1) * diff1 / diff2);
+private:
+  T SecantFormula(T xn, T xn_1) {
+    return xn_1 - (xn_1 - xn) * this->function(xn_1) /
+                      (this->function(xn_1) - this->function(xn));
+    // auto diff1 = xn_minus_1 - xn_minus_2;
+    // auto diff2 = this->function(xn_minus_1) - this->function(xn_minus_2);
+    // if (diff1 < this->epsilon)
+    //   return xn_minus_1;
+    // // if (fabsf(diff2) < this->epsilon) throw std::runtime_error("nan");
+    // return xn_minus_1 - (this->function(xn_minus_1) * diff1 / diff2);
   }
 };
